bbbc60d9b0cb0a22c3414528d8465db57525c383
Sailornina <sylcardozo.sc@gmail.com>
2021-07-19 23:41:53 +0200

    Added Book Class

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-let bookCollection = [];
 
-function handleAddBook() {
-    let addBookForm = document.getElementById('add-book').elements;
-    console.log(addBookForm["read"].value);
+// Clase Book, con un constructor que recibe...
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+
+    this.toggleReadStatus = () => {
+        this.isRead = !this.isRead;
+    }
 }
+
+const booksCollection = [
+    new Book("Rayuela", "Julio Cortazar", 300, true),
+    new Book("War and Peace", "Leo Tolstoy", 280, true),
+    new Book("Little Women", "Louisa May Alcott", 350, true),
+  
+];
+ 
+window.addEventListener('DOMContentLoaded', () => {
+
+});
+
+
