f6f43d2e52ccfb25bf469b3d9ea55ba687d692f6
Sailornina <sylcardozo.sc@gmail.com>
2021-07-21 21:24:32 +0200

    Added localStorage

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+const library = [];
 
-// Clase Book, con un constructor que recibe...
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
@@ -11,15 +11,34 @@ function Book(title, author, pages, isRead) {
     }
 }
 
-const booksCollection = [
+function updateStorage() {
+    localStorage.setItem('BOOK_COLLECTION_KEY', JSON.stringify(bookCollection));
+}
+
+
+let bookCollection = [
     new Book("Rayuela", "Julio Cortazar", 300, true),
     new Book("War and Peace", "Leo Tolstoy", 280, true),
     new Book("Little Women", "Louisa May Alcott", 350, true),
-  
 ];
- 
+
+function addBook(title, author, pages, isRead) {
+    bookCollection.push(new Book(title, author, pages, isRead));
+    updateStorage();
+}
+
+if (!localStorage.getItem('BOOK_COLLECTION_KEY')) {
+    localStorage.setItem('BOOK_COLLECTION_KEY', JSON.stringify(bookCollection));
+} else {
+    bookCollection = JSON.parse(localStorage.getItem('BOOK_COLLECTION_KEY')).map(item => {
+        return new Book(item.title, item.author, item.pages, item.isRead)       
+    });
+}
+
 window.addEventListener('DOMContentLoaded', () => {
 
-});
+    let booksContainer = document.getElementById("books-tiles-container");
 
+    
+});
 
