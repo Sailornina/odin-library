02febd99d7857f84bcd5adbafb150492702a48af
Sailornina <sylcardozo.sc@gmail.com>
2021-07-24 21:12:47 +0200

    Formated index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const library = [];
-
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
@@ -14,7 +12,6 @@ function Book(title, author, pages, isRead) {
 function updateStorage() {
     localStorage.setItem('BOOK_COLLECTION_KEY', JSON.stringify(bookCollection));
 }
-
 
 let bookCollection = [
     new Book("Rayuela", "Julio Cortazar", 300, true),
@@ -31,20 +28,16 @@ if (!localStorage.getItem('BOOK_COLLECTION_KEY')) {
     localStorage.setItem('BOOK_COLLECTION_KEY', JSON.stringify(bookCollection));
 } else {
     bookCollection = JSON.parse(localStorage.getItem('BOOK_COLLECTION_KEY')).map(item => {
-        return new Book(item.title, item.author, item.pages, item.isRead)       
+        return new Book(item.title, item.author, item.pages, item.isRead)
     });
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-
-
     const booksContainer = document.getElementById('books-tiles-container');
 
-
     const fragment = document.createDocumentFragment()
-    
-    bookCollection.forEach((book, i) => {
 
+    bookCollection.forEach((book, i) => {
         const bookTile = document.createElement('div')
         bookTile.classList.add('book-tile')
         fragment.appendChild(bookTile)
@@ -60,11 +53,8 @@ window.addEventListener('DOMContentLoaded', () => {
         const pages = document.createElement('p')
         pages.innerText = book.pages
         fragment.appendChild(pages)
-    
     });
-    
-    booksContainer.appendChild(fragment)
 
-   
+    booksContainer.appendChild(fragment)
 });
 
