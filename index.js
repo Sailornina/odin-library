1599fae670cd90631eb09e7d8e37dc9fb0b71778
Sailornina <sylcardozo.sc@gmail.com>
2021-08-01 18:00:19 +0200

    Implemented refresh bookContainer on new book

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ let bookCollection = [
     new Book("Little Women", "Louisa May Alcott", 350, true),
 ];
 
-function addBook(title, author, pages, isRead) {
-    bookCollection.push(new Book(title, author, pages, isRead));
+function addBook(book) {
+    bookCollection.push(book);
     updateStorage();
 }
 
@@ -35,70 +35,78 @@ if (!localStorage.getItem('BOOK_COLLECTION_KEY')) {
 window.addEventListener('DOMContentLoaded', () => {
 
     const bookForm = document.getElementById('add-book-form');
-    
+    const booksContainer = document.getElementById('books-tiles-container');
+
     bookForm.addEventListener('submit', e => {
         e.preventDefault();
         const { title, author, pages, read} = e.currentTarget.elements;
-        addBook(title.value, author.value, parseInt(pages.value), read.value === 'on');
+        const book = new Book(title.value, author.value, parseInt(pages.value), read.checked);
+        addBook(book);
+        let bookTile = createBookTile(book, bookCollection.length - 1)
+        booksContainer.appendChild(bookTile);
     });
 
 
-    // Book-Card
-    const booksContainer = document.getElementById('books-tiles-container');
+    // Book-Card  
     booksContainer.innerText = "";
 
     const fragment = document.createDocumentFragment();
 
     bookCollection.forEach((book, i) => {
-        const bookTile = document.createElement('div');
-        bookTile.classList.add('book-tile');
-        fragment.appendChild(bookTile);
-
-        const title = document.createElement('p');
-        title.innerText = book.title;
-        bookTile.appendChild(title);
-
-        const author = document.createElement('p');
-        author.innerText = book.author;
-        bookTile.appendChild(author);
-
-        const pages = document.createElement('p');
-        pages.innerText = book.pages;
-        bookTile.appendChild(pages);
-
-        bookTile.dataset.index = i;
-
-        const containerControl = document.createElement('div');
-        containerControl.classList.add('control-book');
-        bookTile.appendChild(containerControl);
-
-        const isRead = book.isRead;
-        bookTile.appendChild(containerControl);
-
-        const label = document.createElement('label');
-        label.classList.add('switch');
-
-        const toggle = document.createElement('input');
-        toggle.classList.add('switch-input');
-        toggle.checked = book.isRead;
-        toggle.type = 'checkbox';
-        toggle.name = 'read'
-        toggle.setAttribute('type', 'checkbox');
-        toggle.addEventListener('change', (e) => {
-            book.toggleReadStatus();
-            updateStorage();
-        });
-        label.appendChild(toggle);
-
-        const span = document.createElement('span');
-        span.classList.add('slider');
-        span.classList.add('round');
-        label.appendChild(span);
-
-        bookTile.appendChild(label);
-
-        booksContainer.appendChild(fragment);
-
+       let bookTile = createBookTile(book, i);
+       fragment.appendChild(bookTile);
     });
 
+    // This is important to prevent reloading the DOM every time we add a book
+    booksContainer.appendChild(fragment);
 });
+
+function createBookTile(book, i) {
+    const bookTile = document.createElement('div');
+    bookTile.classList.add('book-tile');
+
+    const title = document.createElement('p');
+    title.innerText = book.title;
+    bookTile.appendChild(title);
+
+    const author = document.createElement('p');
+    author.innerText = book.author;
+    bookTile.appendChild(author);
+
+    const pages = document.createElement('p');
+    pages.innerText = book.pages;
+    bookTile.appendChild(pages);
+
+    bookTile.dataset.index = i;
+
+    const containerControl = document.createElement('div');
+    containerControl.classList.add('control-book');
+    bookTile.appendChild(containerControl);
+
+    const isRead = book.isRead;
+    bookTile.appendChild(containerControl);
+
+    const label = document.createElement('label');
+    label.classList.add('switch');
+
+    const toggle = document.createElement('input');
+    toggle.classList.add('switch-input');
+    toggle.checked = book.isRead;
+    toggle.type = 'checkbox';
+    toggle.name = 'read';
+    toggle.setAttribute('type', 'checkbox');
+    toggle.addEventListener('change', (e) => {
+        book.toggleReadStatus();
+        updateStorage();
+    });
+    label.appendChild(toggle);
+
+    const span = document.createElement('span');
+    span.classList.add('slider');
+    span.classList.add('round');
+    label.appendChild(span);
+
+    bookTile.appendChild(label);
+
+    return bookTile;
+}
